86dde1f9cca35936cf3343958812d7466933bb59
Amanda Dilworth <amandadilworth@yahoo.com>
2018-12-05 14:27:31 +0900

    added json file to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'JSON'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +8,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+ingredients = JSON.parse(json)
+
+ingredients.each do |ingredient|
+  ingredient = ingredient["strIngredient1"]
+end
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "lime")
 Ingredient.create(name: "ice")
